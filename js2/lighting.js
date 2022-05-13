60ac341b1bd3c45cf6be9f4c5af063144a00de4d
GenRex <richardngo2000@gmail.com>
2022-05-13 15:18:26 +1000

    Update

--- js2/lighting.js
+++ js2/lighting.js
@@ -1,93 +1,94 @@
+let camera, scene, renderer;
 
-			let camera, scene, renderer;
+let sky, sun;
 
-			let sky, sun;
+init();
+render();
 
-			init();
-			render();
+function initSky() {
 
-			function initSky() {
+	// Add Sky
+	sky = new Sky();
+	sky.scale.setScalar( 450000 );
+	scene.add( sky );
 
-				// Add Sky
-				sky = new Sky();
-				sky.scale.setScalar( 450000 );
-				scene.add( sky );
+	sun = new THREE.Vector3();
 
-				sun = new THREE.Vector3();
+	/// GUI
 
-				/// GUI
+	const effectController = {
+		turbidity: 10,
+		rayleigh: 3,
+		mieCoefficient: 0.005,
+		mieDirectionalG: 0.7,
+		elevation: 2,
+		azimuth: 180,
+		exposure: renderer.toneMappingExposure
+	};
 
-				const effectController = {
-					turbidity: 10,
-					rayleigh: 3,
-					mieCoefficient: 0.005,
-					mieDirectionalG: 0.7,
-					elevation: 2,
-					azimuth: 180,
-					exposure: renderer.toneMappingExposure
-				};
+	function guiChanged() {
 
-				function guiChanged() {
+		const uniforms = sky.material.uniforms;
+		uniforms[ 'turbidity' ].value = effectController.turbidity;
+		uniforms[ 'rayleigh' ].value = effectController.rayleigh;
+		uniforms[ 'mieCoefficient' ].value = effectController.mieCoefficient;
+		uniforms[ 'mieDirectionalG' ].value = effectController.mieDirectionalG;
 
-					const uniforms = sky.material.uniforms;
-					uniforms[ 'turbidity' ].value = effectController.turbidity;
-					uniforms[ 'rayleigh' ].value = effectController.rayleigh;
-					uniforms[ 'mieCoefficient' ].value = effectController.mieCoefficient;
-					uniforms[ 'mieDirectionalG' ].value = effectController.mieDirectionalG;
+		const phi = THREE.MathUtils.degToRad( 90 - effectController.elevation );
+		const theta = THREE.MathUtils.degToRad( effectController.azimuth );
 
-					const phi = THREE.MathUtils.degToRad( 90 - effectController.elevation );
-					const theta = THREE.MathUtils.degToRad( effectController.azimuth );
+		sun.setFromSphericalCoords( 1, phi, theta );
 
-					sun.setFromSphericalCoords( 1, phi, theta );
+		uniforms[ 'sunPosition' ].value.copy( sun );
 
-					uniforms[ 'sunPosition' ].value.copy( sun );
+		renderer.toneMappingExposure = effectController.exposure;
+		renderer.render( scene, camera );
 
-					renderer.toneMappingExposure = effectController.exposure;
-					renderer.render( scene, camera );
+	}
 
-				}
+	const gui = new GUI();
 
-				const gui = new GUI();
+	gui.add( effectController, 'turbidity', 0.0, 20.0, 0.1 ).onChange( guiChanged );
+	gui.add( effectController, 'rayleigh', 0.0, 4, 0.001 ).onChange( guiChanged );
+	gui.add( effectController, 'mieCoefficient', 0.0, 0.1, 0.001 ).onChange( guiChanged );
+	gui.add( effectController, 'mieDirectionalG', 0.0, 1, 0.001 ).onChange( guiChanged );
+	gui.add( effectController, 'elevation', 0, 90, 0.1 ).onChange( guiChanged );
+	gui.add( effectController, 'azimuth', - 180, 180, 0.1 ).onChange( guiChanged );
+	gui.add( effectController, 'exposure', 0, 1, 0.0001 ).onChange( guiChanged );
 
-				gui.add( effectController, 'turbidity', 0.0, 20.0, 0.1 ).onChange( guiChanged );
-				gui.add( effectController, 'rayleigh', 0.0, 4, 0.001 ).onChange( guiChanged );
-				gui.add( effectController, 'mieCoefficient', 0.0, 0.1, 0.001 ).onChange( guiChanged );
-				gui.add( effectController, 'mieDirectionalG', 0.0, 1, 0.001 ).onChange( guiChanged );
-				gui.add( effectController, 'elevation', 0, 90, 0.1 ).onChange( guiChanged );
-				gui.add( effectController, 'azimuth', - 180, 180, 0.1 ).onChange( guiChanged );
-				gui.add( effectController, 'exposure', 0, 1, 0.0001 ).onChange( guiChanged );
+	guiChanged();
 
-				guiChanged();
+}
 
-			}
+function init() {
+	const helper = new THREE.GridHelper( 10000, 2, 0xffffff, 0xffffff );
+	scene.add( helper );
+	renderer.outputEncoding = THREE.sRGBEncoding;
+	renderer.toneMapping = THREE.ACESFilmicToneMapping;
+	renderer.toneMappingExposure = 0.5;
 
-			function init() {
+	controls.addEventListener( 'change', render );
+	//controls.maxPolarAngle = Math.PI / 2;
 
-				camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 100, 2000000 );
-				camera.position.set( 0, 100, 2000 );
+	initSky();
 
-				scene = new THREE.Scene();
+	window.addEventListener( 'resize', onWindowResize );
 
-				const helper = new THREE.GridHelper( 10000, 2, 0xffffff, 0xffffff );
-				scene.add( helper );
+}
 
-				renderer = new THREE.WebGLRenderer();
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				renderer.outputEncoding = THREE.sRGBEncoding;
-				renderer.toneMapping = THREE.ACESFilmicToneMapping;
-				renderer.toneMappingExposure = 0.5;
-				document.body.appendChild( renderer.domElement );
+function onWindowResize() {
 
-				const controls = new OrbitControls( camera, renderer.domElement );
-				controls.addEventListener( 'change', render );
-				//controls.maxPolarAngle = Math.PI / 2;
-				controls.enableZoom = false;
-				controls.enablePan = false;
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
 
-				initSky();
+	renderer.setSize( window.innerWidth, window.innerHeight );
 
-				window.addEventListener( 'resize', onWindowResize );
+	render();
 
-			}
+}
 
+function render() {
+
+	renderer.render( scene, camera );
+
+}
