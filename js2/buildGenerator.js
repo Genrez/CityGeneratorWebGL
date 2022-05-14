f73c7061afd3de49cf8efd2a18ded3ac7cdad201
GenRex <richardngo2000@gmail.com>
2022-05-14 15:00:02 +1000

    Update buildGenerator.js

--- js2/buildGenerator.js
+++ js2/buildGenerator.js
@@ -1,10 +1,10 @@
 var buildings = [];
 var numberOfBuildings = 100;
 
-function generateBuilding(model) {
+function generateBuilding(number) {
     var loader = new THREE.PLYLoader();
     var mesh = null;
-    loader.load('assets/building_models/b' + model + '.ply', function (geometry) {
+    loader.load('models/Building' + number + '.ply', function (geometry) {
         var material = new THREE.MeshPhongMaterial();
         //material.color = new THREE.Color(r, g, b);
         material.shininess = 50;
