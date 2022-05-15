99ea32feb996f171a858f73a2a95c0547060469d
GenRex <richardngo2000@gmail.com>
2022-05-15 20:59:23 +1000

    Update

--- js2/buildGenerator.js
+++ js2/buildGenerator.js
@@ -62,24 +62,36 @@ function generateCity() {
 
 function generateBuilding(i, j) {
     var model = Math.floor(Math.random() * 5) + 1;
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
     loader.load('models/Building' + model + '.ply', function (geometry) {
         geometry.computeVertexNormals();
         geometry.computeBoundingBox();
 
-        const material = new THREE.MeshStandardMaterial( { color: 0x0055ff, flatShading: true } );
+        const material = new THREE.MeshStandardMaterial( { color: getRandomColor(), flatShading: true } );
         const mesh = new THREE.Mesh( geometry, material );
         
         mesh.position.x = i;
         mesh.position.z = j;
 
+        //mesh.rotation.y = Math.random() * Math.PI / 2;
+        
         mesh.rotation.x = - Math.PI / 2;
-        mesh.scale.multiplyScalar( 4 );
+        mesh.scale.multiplyScalar( 6 );
         mesh.castShadow = true;
         mesh.receiveShadow = true;
 
         scene.add(mesh);
     });
 }
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 
 generateCity()
 
