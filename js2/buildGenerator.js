cb89db3353af09e72cb9adfc9c369fbba62515d3
GenRex <richardngo2000@gmail.com>
2022-05-15 15:06:08 +1000

    Update

--- js2/buildGenerator.js
+++ js2/buildGenerator.js
@@ -1,30 +1,33 @@
 var numberOfBuildings = 100;
 
-function generateBuilding(number) {
+function generateBuilding(number, x, z) {
     var loader = new THREE.PLYLoader();
     var mesh = null;
     loader.load('models/Building' + number + '.ply', function (geometry) {
         var material = new THREE.MeshPhongMaterial();
         geometry.computeVertexNormals();
+        geometry.computeBoundingBox();
+
         mesh = new THREE.Mesh(geometry, material);
         mesh.name = "building";
-        geometry.computeBoundingBox();
-        var size = geometry.boundingBox.getSize();
-        var sca = new THREE.Matrix4();
-        var combined = new THREE.Matrix4();
-        combined.multiply(sca);
-        mesh.applyMatrix(combined);
+
+        mesh.position.x = x;
+        mesh.position.z = z;
+
         mesh.castShadow = true;
         mesh.receiveShadow = true;
+
         scene.add(mesh);
     });
 }
 
 function generateBase() {
-
+    for (var i = 0; i < numberOfBuildings; i++) {
+        generateBuilding(Math.random(1,5), Math.random(0, 1000), Math.random(0, 1000));
+    }
 }
 
-generateBuilding(1);
+generateBase();
 
 
 
--- js2/gui.js
+++ js2/gui.js
@@ -2,6 +2,10 @@ var gui = new dat.GUI({
     name: "HUD"
 });
  
+var params = {
+    camera_rotation: 0,
+}
+
 var cameraSettings = gui.addFolder('Camera Settings');
 
 var generationSettings = gui.addFolder('Generation Settings');
