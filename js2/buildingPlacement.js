2c40ae09941e37bf3c4314db34281b683bef0949
EdoardoMenon <edoardomenon1997@gmail.com>
2022-05-15 20:29:36 +1000

    Buildings exist

--- js2/buildingPlacement.js
+++ js2/buildingPlacement.js
@@ -16,6 +16,24 @@ function generateGrid() {
     grid.position.y = 1;
     scene.add(grid);
 
+    const buildings = [];
+
+    for (var i = 0; i < 360; i += 36) {
+        for (var j = 0; j < 360; j += 36) {
+            const buildingExists = buildings.find(function(building) {
+                return (building.position.x === i) 
+                && (building.position.z === j)
+            })
+
+            if (!buildingExists) {
+                const building = generateBuilding(i, j);
+                buildings.push(building);
+            }
+        }
+    }
+}
+
+function generateBuilding(x, z) {
     const testBox = new THREE.Mesh(
         new THREE.PlaneGeometry(36, 36),
         new THREE.MeshBasicMaterial({
@@ -23,13 +41,10 @@ function generateGrid() {
         })
     )
 
-    // testBox.rotateX(-Math.PI / 2);
-    // testBox.position.set(0, 1, 0);
-    // scene.add(testBox);
-
     testBox.rotateX(-Math.PI / 2);
-    testBox.position.set(108, 1, -108);
+    testBox.position.set(x, 1, z);
     scene.add(testBox);
+    return testBox;
 }
 
 generateGrid();
