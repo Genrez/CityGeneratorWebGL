6e97956d31d33b8844ce455e6b579867897a534c
GenRex <richardngo2000@gmail.com>
2022-05-15 17:25:15 +1000

    Update

--- js2/buildGenerator.js
+++ js2/buildGenerator.js
@@ -1,19 +1,39 @@
 var numberOfBuildings = 20;
 var loader = new THREE.PLYLoader();
 
-function generateBuilding(number, x, z) {
-    var mesh = null;
+loader.load( './models/dolphins.ply', function ( geometry ) {
+    geometry.computeVertexNormals();
+    geometry.computeBoundingBox();
+
+    const material = new THREE.MeshStandardMaterial( { color: 0x0055ff, flatShading: true } );
+    const mesh = new THREE.Mesh( geometry, material );
+
+    mesh.position.y = 1.5;
+    mesh.position.z = 4;
+    mesh.rotation.x = - Math.PI / 2;
+    mesh.scale.multiplyScalar( 0.007 );
+
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+
+    mesh.name = "loaded_mesh";
+    scene.add( mesh );
+
+} );
+
+
+function generateBuilding(number) {
     loader.load('models/Building' + number + '.ply', function (geometry) {
-        var material = new THREE.MeshPhongMaterial();
         geometry.computeVertexNormals();
         geometry.computeBoundingBox();
 
-        mesh = new THREE.Mesh(geometry, material);
-        mesh.name = "building";
+        const material = new THREE.MeshStandardMaterial( { color: 0x0055ff, flatShading: true } );
+        const mesh = new THREE.Mesh( geometry, material );
+        
+        mesh.position.x = 40;
 
-        mesh.position.x = x;
-        mesh.position.z = z;
-
+        mesh.rotation.x = - Math.PI / 2;
+        mesh.scale.multiplyScalar( 4 );
         mesh.castShadow = true;
         mesh.receiveShadow = true;
 
@@ -21,13 +41,13 @@ function generateBuilding(number, x, z) {
     });
 }
 
-function generateBase() {
-    for (var i = 0; i < numberOfBuildings; i++) {
-        generateBuilding(Math.random(1,5), Math.random(0, 1000), Math.random(0, 1000));
-    }
-}
-
-generateBase();
+//function generateBase() {
+//    for (var i = 0; i < numberOfBuildings; i++) {
+ //       generateBuilding(Math.random(1,5), Math.random(0, 1000), Math.random(0, 1000));
+ //   }
+//}
+var model = 5;
+generateBuilding(model);
 
 
 
